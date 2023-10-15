7a15b960109e0677be14f0508736680ea79744d3
nashuaf <125389723+nashuaf@users.noreply.github.com>
2023-10-15 18:55:19 -0400

    Update queries.sql

---
+++ queries.sql
@@ -0,0 +1,7 @@
+-- This file will contain queries to execute based on the information needed 
+
+-- Get basic information for user
+
+-- Find most recent conversation
+
+-- Get tracked coins for user 
