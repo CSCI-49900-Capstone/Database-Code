30d4b66a1d9d08ef2e8913358e92a2e1c9f5600e
nashuaf <125389723+nashuaf@users.noreply.github.com>
2023-11-19 12:58:13 -0500

    Update userInfo_DML.sql

--- Database Code/Python database code/userInfo_DML.sql
+++ Database Code/Python database code/userInfo_DML.sql
@@ -1,9 +1,13 @@
 -- USER DML FOR PYTHON
 
 -- Get user API key
+SELECT APIKEY FROM userauth INNER JOIN users ON userauth.USERNAME = users.USERNAME WHERE users.USERNAME = (python variable with name);
 
 -- Get user wallet
+SELECT WALLER FROM accounts INNER JOIN users ON accounts.ACCTID = users.ACCTID WHERE users.USERNAME = (python variable with name);
 
 -- Get all transactions for user
+SELECT * FROM trades WHERE trades.USERNAME = (python variable with name);
 
 -- Get value of a certain transaction
+SELECT * FROM trades WHERE trades.USERNAME = (python variable with name) AND trades.USD_AMOUNT > (python variable with trade amount);
