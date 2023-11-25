21918a1b0ccb98a242c8429726613936c5deddf0
nashuaf <125389723+nashuaf@users.noreply.github.com>
2023-11-25 12:56:33 -0500

    Delete queries.sql

--- queries.sql
+++
@@ -1,7 +0,0 @@
--- This file will contain queries to execute based on the information needed 
-
--- Get basic information for user
-
--- Find most recent conversation
-
--- Get tracked coins for user 
